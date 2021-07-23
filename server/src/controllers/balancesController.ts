ab6f9147c12568ae5623aeafbb3100cb4464e904
GianTempo <giantemporetti@gmail.com>
2021-07-23 19:02:43 -0300

    Code refactor on balances controller.

--- server/src/controllers/balancesController.ts
+++ server/src/controllers/balancesController.ts
@@ -2,9 +2,11 @@ import { Request, Response } from 'express';
 
 import pool from '../database';
 
-
-//TO-DO: Seguir con el tutorial para hacer la REST API.
 class BalancesController {
+
+    /**
+     * Function to get all balances from database.
+     */
     public async balances(req: Request, res: Response) {
         const balances = await pool.query('SELECT * FROM balances');
         res.json(balances);
@@ -25,19 +27,44 @@ class BalancesController {
         }
     }
 
+    /**
+     * Function to add a balance to the database.
+     * It recieves an object that contains all the data 
+     */
     public async addBalance(req: Request, res: Response) {
-        await pool.query('INSERT INTO balances set ?', [req.body]);
-        res.json({message: 'Added balance succefully'})
+        if (JSON.stringify(req.body) !== '{}') { //Verify that req body is not an empty object
+            await pool.query('INSERT INTO balances set ?', [req.body]);
+            res.json({ message: 'Added balance succefully' })
+        }
+        else { //If req body is an empty object, return 404 code error
+            res.status(404).json({ text: 'You cant post an empty body' });
+        };
     }
 
+
+    /**
+     * Function to delete a specific balance from database.
+     * It recieves the id of the balance the user wants to delete.
+     */
     public async deleteBalance(req: Request, res: Response) {
         await pool.query('DELETE FROM balances WHERE id = ?', req.params.id);
         res.send('Deleted balance');
     }
 
+
+    /**
+     * Function to update a balance of the database.
+     * It recieves the id of the balance that the user wants to update.
+     * It also recieves the data that has to be modified in the database.
+     */
     public async updateBalance(req: Request, res: Response) {
-        await pool.query('UPDATE balances SET ? WHERE id = ?', [req.body, req.params.id]);
-        res.json({ text: 'Balance updated' })
+        if (JSON.stringify(req.body) !== '{}') { //Verify that req body is not an empty object
+            await pool.query('UPDATE balances SET ? WHERE id = ?', [req.body, req.params.id]);
+            res.json({ text: 'Balance updated' })
+        }
+        else { //If req body is an empty object, return 404 code error
+            res.status(404).json({ text: 'PUT body is empty' });
+        }
     }
 }
  
