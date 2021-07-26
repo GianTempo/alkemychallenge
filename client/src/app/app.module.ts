992f1ca8173f92e784beb7c9213696d9822ecab5
GianTempo <giantemporetti@gmail.com>
2021-07-26 19:18:11 -0300

    Created add transaction form
    Created add transaction form with:
    -Concept
    -Amount
    -Datetime
    -Transaction Type.
    
    -Updated latest transactions table to show transaction id
    -Added date pipe to date column in transactions table to show a more user-friendly date format.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { AddBalanceComponent } from './components/add-balance/add-balance.compon
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [
     BalancesService
--- client/src/app/components/add-balance/add-balance.component.ts
+++ client/src/app/components/add-balance/add-balance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Balance } from 'src/app/models/balance';
+import { BalancesService } from 'src/app/services/balances.service';
 
 @Component({
   selector: 'app-add-balance',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBalanceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bSvc:BalancesService) { }
 
   ngOnInit(): void {
+  }
+
+  balance: Balance = {
+    clientId: 1,
+    concept: '',
+    amount: 0,
+    createdAt: new Date(),
+    transactionType: ''
+  }
+
+  saveTransaction(): void {
+    this.bSvc.saveBalance(this.balance)
+      .subscribe(
+        err => console.log(err),
+        res => console.log(res)
+    )
   }
 
 }
