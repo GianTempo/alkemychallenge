73fbfa477965bc7163b9bd843a9a69a7f730ef88
GianTempo <giantemporetti@gmail.com>
2021-07-28 10:03:00 -0300

    Migrated database to a cloud host.
    -Migrated database to cloud hosting and modified keys to connect to this host.
    -Changes on database query: added foreign key in balances table.

--- database/database.sql
+++ database/database.sql
@@ -2,6 +2,12 @@ CREATE DATABASE alkemy_challenge_db;
 
 USE alkemy_challenge_db;
 
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50)
+);
+
 CREATE TABLE balances (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     clientId INT(11) NOT NULL,
@@ -9,13 +15,5 @@ CREATE TABLE balances (
     amount INT(6),
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     transactionType VARCHAR(180),
+    FOREIGN KEY clientId REFERENCES users(id)
 );
-
-CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(50)
-);
-
-DESCRIBE balances;
-DESCRIBE users;
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,8 +1,8 @@
 export default {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'alkemy_challenge_db'
+        host: 'b0q3letsb8hxlibooklw-mysql.services.clever-cloud.com',
+        user: 'udjn1ynbd5wpqviz',
+        password: 'rdSqsFPAT1oh6uTjv0gJ',
+        database: 'b0q3letsb8hxlibooklw'
     }
 }
