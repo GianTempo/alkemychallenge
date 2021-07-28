9f3ca384440622f9e4a28c62e7b5173cfe4158e3
GianTempo <giantemporetti@gmail.com>
2021-07-28 13:29:08 -0300

    Migrated back to local database
    -Migrated back to locale database as cloud database doesn't work as expected.

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,8 +1,8 @@
 export default {
     database: {
-        host: 'b0q3letsb8hxlibooklw-mysql.services.clever-cloud.com',
-        user: 'udjn1ynbd5wpqviz',
-        password: 'rdSqsFPAT1oh6uTjv0gJ',
-        database: 'b0q3letsb8hxlibooklw'
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'alkemy-challenge_db'
     }
 }
