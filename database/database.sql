d78ce42db72ef6219e7c0a11d8c21e021336a29f
GianTempo <giantemporetti@gmail.com>
2021-07-28 09:44:05 -0300

    Code corrections in database

--- database/database.sql
+++ database/database.sql
@@ -7,15 +7,15 @@ CREATE TABLE balances (
     clientId INT(11) NOT NULL,
     concept VARCHAR(180),
     amount INT(6),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     transactionType VARCHAR(180),
 );
 
-CREATE TABLE users {
+CREATE TABLE users (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50),
     password VARCHAR(50)
-};
+);
 
 DESCRIBE balances;
 DESCRIBE users;
