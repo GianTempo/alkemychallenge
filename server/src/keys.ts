d4712b11bbbfd6686dd44afbadbc85982bd19446
GianTempo <giantemporetti@gmail.com>
2021-08-06 19:15:39 -0300

    Added auth to backend
    -Created auth POST routes to both signup and login to the app.
    -Created auth controller with necessary methods to both signin and login to the app.
    -The API now returns a token to identify the user in the app.
    -Removed index routes and controller since they weren't necessary.
    -Added jsonWebToken and it's types, so it's recommended to run npm install to install both dependencies.
    -Fixed database name on keys.ts since the name was not the right one.

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,6 +3,6 @@ export default {
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: 'alkemy-challenge_db'
+        database: 'alkemy_challenge_db'
     }
 }
